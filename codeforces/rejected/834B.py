ba83744182a8acea8d9fcd67940847209aa94c01
Luis Phelipe <luisphelipecode@gmail.com>
2017-07-30 23:59:36 -0300

    30-07

--- codeforces/rejected/834B.py
+++ codeforces/rejected/834B.py
@@ -3,22 +3,37 @@
 def main():
     n, g = [int(x) for x in input().split()]
     data = input()
-    doors = [0]*n
+    dats = set(data)
+    dic = {}
 
-    for value in set(data):
-        count = data.count(value)
-        trig = False
-        
-        for i in range(n):
-            if data[i] == value:
-                trig = True
-                count -= 1
-            if trig:
-                doors[i] += 1
-            if not count: 
+    for val in dats:
+        dic[val] = [data.index(val), 0]
+
+        for end in range(n-1, 0, -1):
+            if data[end] == val:
+                dic[val][1] = end
                 break
 
-    print("YES" if max(doors) > g else "NO")
+    inicio = sorted([dic[key][0] for key in dic])
+    final = sorted([dic[key][1] for key  in dic])
+    
+    maximo = 0
+    total = 0
+    i, f = 0, 0
+
+    while (i < len(inicio)):
+        if (inicio[i] <= final[f]):
+            total += 1
+            i += 1
+        else:
+            total -= 1
+            f += 1
+
+        if total > maximo:
+            maximo = total
+
+
+    print("YES" if maximo > g else "NO")
 
 if __name__ == "__main__": main()
 
