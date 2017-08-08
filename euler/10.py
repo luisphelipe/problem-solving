27100d5640088a40fe25d7b87e2835a21033cd96
Luis Phelipe <luisphelipecode@gmail.com>
2017-08-07 21:28:52 -0300

    Daily 0807

---
+++ euler/10.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+def isPrime(val):
+    if val < 2:
+        return False
+    for i in range(2, int(val**.5+1)):
+        if not val%i:
+            return False
+    return True
+
+
+def sievSum(val):
+    data = list(range(val+1))
+    data[1] = 0
+    for q in data[2:int(val**.5+1)]:
+        if isPrime(q):
+            w = 2*q
+            while w <= val:
+                data[w] = 0
+                w += q
+    
+    return sum(data)
+
+
+def main():
+    print(sievSum(2000000)) 
+
+
+if __name__ == "__main__": main()
